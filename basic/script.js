97096862f1be37eaadf597ea18fef727bdcde36b
Dmytro Orikhovskyi <d>
2020-05-25 22:13:32 +0300

    first commit

---
+++ basic/script.js
@@ -0,0 +1,19 @@
+// -- Type of data --
+
+/*
+    Number
+    String
+    Boolean
+    Object
+    Null
+    Undefined
+ */
+
+var number = 42;
+var string = 'string message';
+var bool = true;
+var object = {a: 1};
+var nothing = null;
+var undef = undefined;
+
+console.log(typeof undef)
