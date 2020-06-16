526497cffb792d048e34f954c0bd347c018bb769
Dmytro Orikhovskyi <d>
2020-06-16 19:39:59 +0300

    object 2

--- objects/script.js
+++ objects/script.js
@@ -130,4 +130,107 @@
 
 // now user = { name: "John", canView: true, canEdit: true }
 }
+// Значение this – это объект «перед точкой», который использовался для вызова метода.
+{
+    let user = { name: "Джон" };
+    let admin = { name: "Админ" };
+
+    function sayHi() {
+        console.log( this.name );
+    }
+
+// используем одну и ту же функцию в двух объектах
+    user.f = sayHi;
+    admin.f = sayHi;
+    user.f(); // Джон  (this == user)
+    admin.f(); // Админ  (this == admin)
+}
+// У стрелочных функций нет «this»
+{
+    let user = {
+        firstName: "Илья",
+        sayHi() {
+            let arrow = () => console.log(this.firstName);
+            arrow();
+        }
+    };
+
+    user.sayHi(); // Илья
+}
+// Symbol.toPrimitive  transformation a object to primitive type
+{
+    /*
+        - string
+        - number
+        - default
+     */
+    let user = {
+        name: "John",
+        money: 1000,
+
+        [Symbol.toPrimitive](hint) {
+            console.log(`hint: ${hint}`);
+            return hint == "string" ? `{name: "${this.name}"}` : this.money;
+        }
+    };
+
+// демонстрация результатов преобразований:
+    console.log(user.toString()); // hint: string -> {name: "John"}
+    console.log(+user); // hint: number -> 1000
+    console.log(user + 500); // hint: default -> 1500
+}
+{
+    let user = {
+        name: "John",
+        money: 1000,
+
+        // для хинта равного "string"
+        toString() {
+            return `{name: "${this.name}"}`;
+        },
+
+        // для хинта равного "number" или "default"
+        valueOf() {
+            return this.money;
+        }
+
+    };
+
+    console.log(user.toString()); // toString -> {name: "John"}
+    console.log(+user); // valueOf -> 1000
+    console.log(user + 500); // valueOf -> 1500
+
+    /*
+    для примитимов создаётся специальный «объект-обёртка»,
+    который предоставляет нужную функциональность, а после удаляется.
+    Каждый примитив имеет свой собственный «объект-обёртку»,
+    которые называются: String, Number, Boolean и Symbol. Таким образом, они имеют разный набор методов.
+ */
+}
+// Функция-конструктор
+{
+    function User(name) {
+        this.name = name;
+        this.isAdmin = false;
+        this.sayHi = function() {
+            console.log( "Меня зовут: " + this.name );
+        };
+    }
+
+    let user = new User("Вася");
+
+    console.log(user.name); // Вася
+    console.log(user.isAdmin); // false
+    user.sayHi(); // Меня зовут: Вася
+}
+{
+    function User() {
+        this.name = 'John';
+        this.isAdmin = false;
+    }
+
+    let user = new User; // <-- без скобок
+    // то же, что и
+    let user1 = new User();
+}
 
