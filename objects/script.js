8f904354b729e9329b82cf87c0147bfa508a3e84
Dmytro Orikhovskyi <d>
2020-08-03 22:19:14 +0300

    Iterator

--- objects/script.js
+++ objects/script.js
@@ -233,4 +233,44 @@
     // то же, что и
     let user1 = new User();
 }
+// Перебираемые объекты
+{
+    // Перебираемые (или итерируемые) объекты – это концепция, которая позволяет использовать
+    // любой объект в цикле for..of
+
+    let range = {
+        from: 1,
+        to: 5
+    };
+
+    // 1. вызов for..of сначала вызывает эту функцию
+    range[Symbol.iterator] = function() {
+
+        // ...она возвращает объект итератора:
+        // 2. Далее, for..of работает только с этим итератором, запрашивая у него новые значения
+        return {
+            current: this.from,
+            last: this.to,
+
+            // 3. next() вызывается на каждой итерации цикла for..of
+            next() {
+                // 4. он должен вернуть значение в виде объекта {done:.., value :...}
+                if (this.current <= this.last) {
+                    return { done: false, value: this.current++ };
+                } else {
+                    return { done: true };
+                }
+            }
+        };
+    };
+
+    // теперь работает!
+    for (let num of range) {
+        console.log(num); // 1, затем 2, 3, 4, 5
+    }
+}
+// Map
+{
+    // Map
+}
 
