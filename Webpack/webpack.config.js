291b80ad3e507336f82492e5a3c82bdc3cf56423
Dmytro Orikhovskyi <d>
2020-09-26 21:43:33 +0300

    add minimize assets

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,18 +11,28 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
 
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCssAssetsPlugin({})
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
   ],
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
