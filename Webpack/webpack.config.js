5eab42b0c4309fe935e369757ff5c890b3c13aea
Dmytro Orikhovskyi <d>
2020-09-26 22:19:24 +0300

    babel

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -40,6 +40,16 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
 }
