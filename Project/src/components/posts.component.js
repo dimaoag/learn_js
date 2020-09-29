1e7f0857ddf99c09c0a566b9264c4b2dc9c5aece
Dmytro Orikhovskyi <d>
2020-09-29 14:34:42 +0300

    show posts

--- Project/src/components/posts.component.js
+++ Project/src/components/posts.component.js
@@ -1,6 +1,7 @@
 import {Component} from '../core/component'
 import {apiService} from '../services/api.service'
 import {TransformService} from '../services/transform.service'
+const dateFormat = require('dateformat')
 
 export class PostsComponent extends Component {
   constructor(id) {
@@ -11,6 +12,37 @@ export class PostsComponent extends Component {
 
   async onShow() {
     const posts = TransformService.transformPosts(await apiService.getPosts())
-    console.log(posts)
+    const html = posts.map(post => renderPost(post))
+    this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
   }
+
+  onHide() {
+    this.$el.innerHTML = ''
+  }
+
+}
+function renderPost(post){
+  const tag = post.type === 'news'
+    ? `<li class="tag tag-blue tag-rounded">Новость</li>`
+    : `<li class="tag tag-rounded">Заметка</li>`
+
+  const button = `<button class="button-round button-small button-primary">Сохранить</button>`
+
+  return `
+    <div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${post.title}</p>
+        <ul class="tags">
+          ${tag}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${post.text}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${dateFormat(post.date, 'yyyy-mm-dd')}</small>
+        ${button}
+      </div>
+    </div>
+  `
 }
